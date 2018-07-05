7714774d035b849ae0898e16be871bc05c219dc7
Steven Lum <stevielum1@gmail.com>
2018-07-05 12:12:08 -0700

    Finish queries 01

---
+++ movie_buff/movie_buff/02_queries.rb
@@ -0,0 +1,55 @@
+# == Schema Information
+#
+# Table name: actors
+#
+#  id          :integer      not null, primary key
+#  name        :string
+#
+# Table name: movies
+#
+#  id          :integer      not null, primary key
+#  title       :string
+#  yr          :integer
+#  score       :float
+#  votes       :integer
+#  director_id :integer
+#
+# Table name: castings
+#
+#  id          :integer      not null, primary key
+#  movie_id    :integer      not null
+#  actor_id    :integer      not null
+#  ord         :integer
+
+def eighties_b_movies
+  # List all the movies from 1980-1989 with scores falling between
+  # 3 and 5 (inclusive).
+  # Show the id, title, year, and score.
+
+end
+
+def bad_years
+  # List the years in which a movie with a rating above 8 was not released.
+
+end
+
+def cast_list(title)
+  # List all the actors for a particular movie, given the title.
+  # Sort the results by starring order (ord). Show the actor id and name.
+
+end
+
+def vanity_projects
+  # List the title of all movies in which the director also appeared
+  # as the starring actor.
+  # Show the movie id and title and director's name.
+
+  # Note: Directors appear in the 'actors' table.
+
+end
+
+def most_supportive
+  # Find the two actors with the largest number of non-starring roles.
+  # Show each actor's id, name and number of supporting roles.
+
+end
